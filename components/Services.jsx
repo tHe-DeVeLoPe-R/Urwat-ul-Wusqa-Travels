7198a05bc0be2a926064cfafe2c68f324e1e027f
saad zafar <codewithsaadzafar@gmail.com>
2024-12-19 15:31:42 +0500

    UI chnages services section

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from '../styles/Services.module.css'
-
+import transport from '../public/images/transport.svg'
+import zyarat from '../public/images/zyarat.svg'
+import guide from '../public/images/guide.svg'
+import realtime from '../public/images/realtime.svg'
 import { useState, useEffect } from 'react'
 export default function Services() {
     const [isNotMobile, setIsNotMobile] = useState(false);
@@ -11,11 +14,12 @@ export default function Services() {
     return (
         <div>
             <div> <h1 className = {isNotMobile? styles.services_heading : styles.mobile_ser_heading}>Round-Trip Services</h1></div>
-            <div> <small className = {styles.services_small}>Click Images To Reveal Services</small></div>
+            <div> <small className = {styles.services_small}>SERVICES FOR GROUP TRAVELING</small></div>
            <div className={isNotMobile ? styles.services_desktop : styles.services_mobile}>
                    
                     <div className={isNotMobile? styles.service_item : styles.mobile_service_item}>
                         <div className={isNotMobile?styles.service_item_content:styles.mobile_service_item_content}>
+                            <img src={transport.src} alt="img" />
                             <h1>Transport</h1>
                           
                              <ol><li>
@@ -37,6 +41,7 @@ export default function Services() {
 
                     <div className={isNotMobile? styles.service_item : styles.mobile_service_item}>
                         <div className={isNotMobile?styles.service_item_content:styles.mobile_service_item_content}>
+                        <img src={zyarat.src} alt="img" />
                             <h1>Zyarat</h1>
                         
                             <ol><li>
@@ -58,6 +63,7 @@ export default function Services() {
 
                      <div className={isNotMobile? styles.service_item : styles.mobile_service_item}>
                         <div className={isNotMobile?styles.service_item_content:styles.mobile_service_item_content}>
+                        <img src={guide.src} alt="img" />
                             <h1>Guiding Session</h1>
                            
                             <p>For Guiding session all the participants of the group will be called to Jaranwala Offiece, where a proper guidance related to Ehram, Manasik and other related things will be given. Your Tickets, Passport and other related documents will be handed over to you in this sesison. In this session you can ask any related queries and any other document related questions. <br />
@@ -70,6 +76,7 @@ export default function Services() {
 
                     <div className={isNotMobile? styles.service_item : styles.mobile_service_item}>
                         <div className={isNotMobile?styles.service_item_content:styles.mobile_service_item_content}>
+                        <img src={realtime.src} alt="img" />
                             <h1>Realtime Assistance</h1>
                             
                             <p>Our CEO, who understands the importance of personal engagement, believes in the power of hands-on guidance. To this end, they travel with the group or personally welcome the passengers at their destination to ensure a smooth transition into their spiritual journey. This unique approach allows for real-time support, fostering a deeper connection with the passengers and enabling a more fulfilling experience.
