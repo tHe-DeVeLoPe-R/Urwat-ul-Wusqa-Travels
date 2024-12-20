7900083a43f1c981e11dce26535a9b928257c579
saad zafar <codewithsaadzafar@gmail.com>
2024-12-20 15:15:53 +0500

    adding map

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -47,8 +47,20 @@ export default function Footer() {
           <ul className={styles.footer_legal_links}>
             <li><a href="#privacy-policy">Privacy Policy</a></li>
             <li><a href="#terms-conditions">Terms & Conditions</a></li>
-            <li><a href="#sitemap">Sitemap</a></li>
+            
           </ul>
+          <h3>Find us on Map</h3>
+          <div style={{ textAlign: 'center', margin: '20px 0' }}>
+      <iframe 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3407.8116938954295!2d73.41621817625042!3d31.336573174298742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39227f209993df4f%3A0xd297765b3b58f0c4!2sAli%20Plaza!5e0!3m2!1sen!2s!4v1734689325952!5m2!1sen!2s" 
+        width="70%" 
+        height="250px" 
+        style={{ border: 0 }} 
+        allowFullScreen 
+        loading="lazy" 
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
         </div>
       </footer>
     </>
